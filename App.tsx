405246d283fb9e2aed94d743040b0eda59409ade
Joey Clapton <joeyclapton42@gmail.com>
2023-11-22 11:07:07 -0300

    feat: create repo from use image picker example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react, { useState } from "react";
+import { Button, Image, View } from "react-native";
+// Vamos importar a biblioteca
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const [image, setImage] = useState<string>();
+
+  const pickImage = async () => {
+    // Nenhuma permissão é necessária para abrir a galeria
+    const result = await ImagePicker.launchImageLibraryAsync({
+      // A propriedade mediaTypes define qual tipo de arquivo é permitido
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+
+      // Quality define a qualidade da imagem, aceita valores de 0-1,
+      // 0 menor qualidade/tamanho e 1 é uma imagem de maior qualidade/tamanho
+      quality: 1,
+    });
+
+    console.log(result);
+
+    // Vamos receber a imagem caso o usuário não feche a galeria de fotos.
+    if (!result.canceled) {
+      const { uri } = result.assets[0];
+      setImage(uri);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image ? (
+        <Image
+          source={{ uri: image as string }}
+          style={{ width: 200, height: 200 }}
+        />
+      ) : null}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
